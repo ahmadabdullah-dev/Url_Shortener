026d603651c9ae0305b61335363af0f8b9b14293
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-08-18 01:21:53 +0300

    feat(api:click): Remove IpAddress, UserAgent, Refferrer and country fields form Click and run the migration

--- DataAccess/Entities/Click.cs
+++ DataAccess/Entities/Click.cs
@@ -4,10 +4,5 @@ public class Click
     public string ClickId { get; set; } = Guid.NewGuid().ToString();
     public string UrlId { get; set; } = null!;
     public DateTime ClickedAt { get; set; }
-    public string? IpAddress { get; set; }
-    public string? UserAgent { get; set; }
-    public string? Referrer { get; set; }
-    public string? Country { get; set; }
-
     public Url Url { get; set; } = null!;
 }
